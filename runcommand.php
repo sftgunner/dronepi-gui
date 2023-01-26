76e8ec32ebcbf622d4a6a325784b816ed4d30bf6
Sebastian Gunner <git@sftgunner.com>
2023-01-26 17:57:59 +0000

    Add passthrough commands

--- runcommand.php
+++ runcommand.php
@@ -2,13 +2,14 @@
 
 // Yes, I know this is 100% not the correct way of doing this
 
-if (isset($_GET['command'])){
-
-if ($_GET['command'] == "test"){
-
-$output = passthru("python3 python/test.py");
-
-}
+if (isset($_POST['command'])){
+    if ($_POST['command'] == "test"){
+        $output = passthru("python3 python/test.py");
+    }
+    elseif ($_POST['command'] == "camerapalette"){
+        $output = passthru("v4l2-ctl -c lep_cid_vid_lut_select=1");
+    }
+    echo $output;
 }
 else{
     echo 'No command';
