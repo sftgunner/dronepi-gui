224018b47be9e71894e3594a7b132735f0ad8a16
Sebastian Gunner <git@sftgunner.com>
2023-01-26 20:59:09 +0000

    Add alternative colour options

--- runcommand.php
+++ runcommand.php
@@ -7,11 +7,15 @@ if (isset($_POST['command'])){
         $output = passthru("python3 python/test.py");
         echo $output;
     }
-    elseif ($_POST['command'] == "camerapalette"){
-        echo exec("whoami");
-        echo "palette";
+    elseif ($_POST['command'] == "palette_fusion"){
         // $output = exec("v4l2-ctl -c lep_cid_vid_lut_select=1");
         exec('v4l2-ctl -c lep_cid_vid_lut_select=1 2>&1', $output);
+        print_r($output);
+        // echo $output; 
+    }
+    elseif ($_POST['command'] == "palette_icefire"){
+        // $output = exec("v4l2-ctl -c lep_cid_vid_lut_select=1");
+        exec('v4l2-ctl -c lep_cid_vid_lut_select=6 2>&1', $output);
         print_r($output);
         // echo $output; 
     }
@@ -22,8 +26,6 @@ if (isset($_POST['command'])){
 }
 else{
     echo 'No command';
-    $output = exec("ping 1.1.1.1 -t 1");
-    echo $output;
 }
 
 ?>
