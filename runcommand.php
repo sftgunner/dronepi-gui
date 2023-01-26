c1851e4d2c859b4da86d1027b983653ff8acf2d0
Sebastian Gunner <git@sftgunner.com>
2023-01-26 18:27:35 +0000

    Update runcommand.php

--- runcommand.php
+++ runcommand.php
@@ -8,9 +8,10 @@ if (isset($_POST['command'])){
         echo $output;
     }
     elseif ($_POST['command'] == "camerapalette"){
+        echo exec("whoami");
+        echo "palette";
         $output = exec("v4l2-ctl -c lep_cid_vid_lut_select=1");
-        echo $output;
-        // echo "palette";
+        echo $output; 
     }
     else{
         print_r($_POST);
